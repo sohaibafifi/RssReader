b18166362f2fc3045492b5005e2734fc147bb59c
Sohaib LAFIFI <sohaib.lafifi@univ-artois.fr>
2020-03-04 12:20:38 +0100

    Change floating btn

--- app/src/main/java/fr/univartois/myapplication/MainActivity.java
+++ app/src/main/java/fr/univartois/myapplication/MainActivity.java
@@ -21,12 +21,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton load_btn = findViewById(R.id.load_btn);
+        load_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Loading news", Snackbar.LENGTH_LONG).show();
             }
         });
     }
